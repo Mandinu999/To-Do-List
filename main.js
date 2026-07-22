c32c6826de7538a7edd03f76bb7eca31888b4731
Mandinu999 <saditha707@gmail.com>
2026-07-22 16:37:37 +0530

    Update Pomodoro timer UI and remove background settings

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, ipcMain, Tray, Menu } = require('electron');
 const path = require('path');
 
+let tray = null;
+let win = null;
+let isQuitting = false;
+
 function createWindow() {
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 950,
     height: 650,
     frame: false,
@@ -16,17 +20,25 @@ function createWindow() {
 
   win.loadFile('index.html');
 
-
-
   ipcMain.on('win-minimize', () => win.minimize());
   ipcMain.on('win-maximize', () => win.isMaximized() ? win.unmaximize() : win.maximize());
-  ipcMain.on('win-close', () => win.close());
+  ipcMain.on('win-close', () => {
+    win.hide();
+  });
+
+  win.on('close', (event) => {
+    if (!isQuitting) {
+      event.preventDefault();
+      win.hide();
+    }
+  });
 
   ipcMain.on('show-notification', (event, { title, body, taskId }) => {
     const { Notification } = require('electron');
     if (Notification.isSupported()) {
       const notif = new Notification({ title, body });
       notif.on('click', () => {
+        if (!win.isVisible()) win.show();
         if (win.isMinimized()) win.restore();
         win.focus();
         event.reply('notification-clicked', taskId);
@@ -36,7 +48,33 @@ function createWindow() {
   });
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  createWindow();
+
+  // Create tray
+  tray = new Tray(path.join(__dirname, 'icon.png')); // Needs an icon.png, electron will fallback if missing/invalid sometimes, but better to handle. 
+  // We'll just use a native image if no icon is available.
+  const { nativeImage } = require('electron');
+  // Create a blank icon if icon.png doesn't exist
+  const icon = nativeImage.createEmpty();
+  tray.setImage(icon);
+  
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Show App', click: () => { win.show(); win.focus(); } },
+    { type: 'separator' },
+    { label: 'Quit', click: () => { isQuitting = true; app.quit(); } }
+  ]);
+  
+  tray.setToolTip('Liquid To-Do');
+  tray.setContextMenu(contextMenu);
+  
+  tray.on('click', () => {
+    if (!win.isVisible()) {
+      win.show();
+    }
+    win.focus();
+  });
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
